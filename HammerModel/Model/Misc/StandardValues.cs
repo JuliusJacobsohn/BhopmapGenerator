8326d7db55e0ace56b830b87bf5c7bbe937bcb93
Julius <julius.jacobsohn@hotmail.com>
2016-10-11 12:55:02 +0200

    Added class to store standard values and sizes

---
+++ HammerModel/Model/Misc/StandardValues.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HammerModel.Model.Misc
+{
+    public class StandardValues
+    {
+        public const int WALL_SIZE = 8;
+        public const double BOTTOM_PERECENTAGE = 0.8;
+    }
+}
