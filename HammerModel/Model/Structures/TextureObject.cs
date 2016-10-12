6584b15979862adc6c20a690c9cd20c69f3060f0
Julius <julius.jacobsohn@hotmail.com>
2016-10-12 19:08:10 +0200

    Cleaned up

---
+++ HammerModel/Model/Structures/TextureObject.cs
@@ -0,0 +1,9 @@
+﻿using HammerModel.Model.Textures;
+
+namespace HammerModel.Model.Structures
+{
+    public abstract class TextureObject : WorldObject
+    {
+        public TexturePack TexturePack { get; set; }
+    }
+}
